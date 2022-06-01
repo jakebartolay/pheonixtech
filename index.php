ef8640e22b08d97c2fbeacdfa1b1cccc46cd4ad2
Mouse <68772728+zukameku@users.noreply.github.com>
2022-06-02 05:37:10 +0800

    Creating Example Pages

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>PHEONIXTECH</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
 
-    echo"WELCOME TO MY PHEONIXTECH";
+<body>
+
+<?php 
+include 'content/section.php';
 ?>
+</body>
+
+</html>
